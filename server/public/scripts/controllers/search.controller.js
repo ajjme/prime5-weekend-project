65513be072c4c791469eedf0f50bdf04bad24bfa
Andrew Johnson <ajj@byu.net>
2018-01-27 15:00:53 -0600

    Dropdown adjusts search

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,3 +1,28 @@
-swApp.controller('SearchController', function() {
+swApp.controller('SearchController', ['$http', function($http) {
     const self = this;
-});
+
+    self.options = [
+        { display: 'Films', term: 'films' },
+        { display: 'People', term: 'people' },
+        { display: 'Planets', term: 'planets' },
+        { display: 'Species', term: 'species' },
+        { display: 'Starships', term: 'starships' },
+        { display: 'Vehicles', term: 'vehicles' }
+    ];
+    self.queryResponse = {};
+
+    self.query = function(name) {
+        if(name) {
+            $http.get('https://swapi.co/api/' + name)
+            .then(response => {
+                self.queryResponse = response.data;
+                console.log('response', self.queryResponse);
+            })
+            .catch(error => {
+                console.log('error', error);
+            })
+        }
+    };
+    self.query();
+
+}]);
