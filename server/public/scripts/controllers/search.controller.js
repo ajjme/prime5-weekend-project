046490ba0769fb4e76580801abafce879f889dc1
Andrew Johnson <ajj@byu.net>
2018-01-27 16:24:02 -0600

    Basic displays for each type

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -16,17 +16,17 @@ swApp.controller('SearchController', ['$http', function($http) {
     self.query = function(category, keywords) {
         const config = { params: { search: keywords } };
         console.log(category, keywords);
-        if(category && keywords) {
+        if(category) {
             $http.get('https://swapi.co/api/' + category, config)
             .then(response => {
+                self.queryResponseType = category;
                 self.queryResponse = response.data;
-                console.log('response', self.queryResponse);
+                console.log(response.data);
             })
             .catch(error => {
                 console.log('error', error);
             })
         }
     };
-    self.query();
 
 }]);
